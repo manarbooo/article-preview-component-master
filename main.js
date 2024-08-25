08732e12143a0e677a1d178bd0ae5bf72a67993b
Manar-boumendjel <boumenamal@gmail.com>
2024-08-25 16:36:40 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const shareSec = document.querySelector(".sharesection");
+const shareBtn = document.querySelector(".shareBtn");
+
+shareBtn.addEventListener('click', () => {
+
+    shareSec.classList.toggle('show');
+});
