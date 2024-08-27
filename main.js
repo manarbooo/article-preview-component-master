0cb0c4f829063767414658da8316804fa6bcf103
Manar-boumendjel <boumenamal@gmail.com>
2024-08-27 16:39:04 +0200

    article preview component master

--- main.js
+++ main.js
@@ -4,4 +4,16 @@ const shareBtn = document.querySelector(".shareBtn");
 shareBtn.addEventListener('click', () => {
 
     shareSec.classList.toggle('show');
+
+    shareBtn.classList.toggle('active');
+
+    if (shareBtn.classList.contains('active')) {
+        // Apply hover styles when the share section is active
+        shareBtn.style.backgroundColor = 'var(--Desaturated_Dark_Blue)';
+        shareBtn.querySelector('.shareicon path').style.fill = 'white';
+    } else {
+        // Revert to default styles when the share section is hidden
+        shareBtn.style.backgroundColor = 'var(--Light_Grayish_Blue)';
+        shareBtn.querySelector('.shareicon path').style.fill = 'var(--Desaturated_Dark_Blue)';
+    }
 });
